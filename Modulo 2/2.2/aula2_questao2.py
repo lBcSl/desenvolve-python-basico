059d3f2937611ec7b4808f06c39dcdde991786c3
BcS <166674570+lBcSl@users.noreply.github.com>
2024-05-09 12:32:41 -0300

    Add files via upload

---
+++ Modulo 2/2.2/aula2_questao2.py
@@ -0,0 +1,17 @@
+#Questao 02
+texto = "o resultado e"
+valor01 = 10
+valor02 = 3.5
+resultado = valor01 + valor02
+
+print(texto)
+print(type(texto))
+
+print(valor01)
+print(type(valor01))
+
+print(valor02)
+print(type(valor02))
+
+print(resultado)
+print(type(resultado))
