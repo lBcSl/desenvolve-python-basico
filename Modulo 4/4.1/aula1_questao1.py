512b19229aa95f1718b1e72c18aa0922991b402f
BcS <166674570+lBcSl@users.noreply.github.com>
2024-05-17 15:14:33 -0300

    Add files via upload

---
+++ Modulo 4/4.1/aula1_questao1.py
@@ -0,0 +1,8 @@
+#Questão 01
+
+x = int(input("Digite um valor"))
+
+if x > 5:
+    print("Maior que 5")
+else:
+    print("Fim")
---
+++ Modulo 4/4.1/aula1_questao2.py
@@ -0,0 +1,7 @@
+#Questão 02
+x = int(input("Digite um valor para x"))
+cont = 0 
+while x < cont:
+    cont = cont + 1
+    print(cont)
+print("fim")
