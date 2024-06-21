1ea43df2ccf1523f24554e236524b7d80527c19f
BcS <166674570+lBcSl@users.noreply.github.com>
2024-06-21 14:19:30 -0300

    Delete Modulo 6/6.4 directory

--- Modulo 6/6.4/aula4_questao3.py
+++
@@ -1,9 +0,0 @@
-#QUESTAO 03
-
-horas_trabalhadas = [40, 37, 45, 40, 40, 48]
-ganho_por_hora = 20
-hora_extra = 25
-
-pagamentos = [ganho_por_hora * min(hora, 40) + hora_extra * max(0, hora - 40) for hora in horas_trabalhadas]
-
-print("Pagamentos:", pagamentos)
